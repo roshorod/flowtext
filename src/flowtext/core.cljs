a9b78328f2e1e1590fef503a4bb24ac38b1ca350
roshorod <homehctop@gmail.com>
2022-03-23 01:41:15 +0200

    feat(parse): Render tokens and format it.

--- src/flowtext/core.cljs
+++ src/flowtext/core.cljs
@@ -1,6 +1,6 @@
 (ns ^:figwheel-hooks flowtext.core
   (:require [cljs.spec.alpha :as s]
-            [clojure.string :refer [split]]
+            [clojure.string :refer [split replace]]
             [rum.core :as rum]
             [store.core :as store]
             [store.mixin :as mixin]))
@@ -12,8 +12,14 @@
 (def token-class "line__token")
 
 (defn make-element [^String content]
-  (let [element (.createElement js/document "span")]
-    (set! (.-textContent element) content)
+  (let [element (.createElement js/document "span")
+        content (replace content #"&nbsp;" " ")]
+    (if (= content "<br>")
+      (do (set! (.-textContent element) "")
+          (.appendChild element
+                        (.createElement js/document "br")))
+      (set! (.-textContent element) content))
+    (set! (.-className element) token-class)
     element))
 
 (defn make-empty-line [target]
@@ -35,60 +41,55 @@
 (def line-string "<div class=\"editor__line\"><span class=\"line__token\">one</span><span class=\"line__token\">two</span></div>")
 
 (def token-regex #"<span class=\"line__token\">.*?</span>")
-
 (def content-regex #"(?<=\>).*(?=\<)")
 
 (def get-line-tokens #(re-seq token-regex %))
 (def get-token-content #(re-seq content-regex %))
 
+(defn token-map-to-line [line map]
+  (doseq [token map]
+    (.appendChild line (make-element token))))
+
 (defn transform-line [line]
-  (doseq [token (get-line-tokens (.-outerHTML line))]
-    (let [content (get-token-content token)]
-      (prn content))))
+  (let [tokens (get-line-tokens (.-outerHTML line))
+        _      (.replaceChildren line)]
+    (doseq [token tokens]
+      (let [content (get-token-content token)
+            token-map (split (clj->js content) #" ")]
+        (token-map-to-line line token-map)))))
+
+(def timer (atom nil))
 
 (defn load-editor [editor]
   (.addEventListener
     editor
     "keyup"
     (fn [event]
-      (let [current-line (.getSelection  js/window)
+      (let [current-line (.getSelection js/window)
             current-node (.-focusNode current-line)]
-        ;; If false:
-        ;;; Make impossible replacing contenteditable node
-        ;; If true:
-        ;;; Make text tokens.
-        (if-let
-            [not-editor-node
-             (not
-               (and (= editor current-node)
-                    ;; if after empty line exist other line,
-                    ;; that prevent adding empty line,
-                    ;; when delete first line
+        (js/clearTimeout @timer)
+        (swap!
+          timer
+          #(js/setTimeout
+             (fn []
+               (if-let
+                   [not-editor-node
                     (not
-                      (= (.-innerHTML current-node) "<br>"))))]
-          (transform-line (token->line current-node))
-          ;; (js/console.log "Tokenize")
-          ;; (js/console.log (clj->js current-line))
-          ;; (js/console.log (token->line current-node))
-          ;; (js/console.log (.-parentElement current-node))
-          (make-empty-line editor))
-        
-        
-        ;; Take until enter was pressed or time out of 2-5 sec.
-        ;; https://stackoverflow.com/questions/4220126/run-javascript-function-when-user-finishes-typing-instead-of-on-key-up
-        
-        ;; Then take outerHTML form line parse it and add child elements
-        
-        ;; May make content checking of old string and new and old tokens,
-        ;; That may make elements without re-rendering.
-   
-        )))
+                      (and (= editor current-node)
+                           ;; if after empty line exist other line,
+                           ;; that prevent adding empty line,
+                           ;; when delete first line
+                           (not
+                             (= (.-innerHTML current-node) "<br>"))))]
+                 (transform-line (token->line current-node))
+                 (make-empty-line editor)))
+             5000)))))
   (set! (.-contentEditable editor) true))
 
 (rum/defc editor-layout []
   [:div#editor
    [:div.editor__line
-    [:span.line__token "One two "]
+    [:span.line__token "One two"]
     [:span.line__token "three"]]])
 
 (defn post-render []
